15b61f9b300c0885956ffbf37a78aff239e9296b
Rizwan Asif <rizasif@gmail.com>
2018-06-02 22:03:49 +0500

    comments

--- createModel.py
+++ createModel.py
@@ -5,14 +5,48 @@ import numpy as np
 import traceback
 import sys
 
+# The following code is run whenever this python script is instantiated
+# i.e either run or imported
+# These following variables have no function block and are thus global variables
+
+# The list of folders to be trained
 folders = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 			 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 			 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+# A string to represent working directory. "." means the current directory/folder.
 root = "."
+
+# The folder to get images to train from
 datasetFolder = "/Data/training"
 
+# Total number of elements or charachters (A-Z, 0-9), which will equal to 36
 TOTAL_ELEMENTS = len(folders) #36
+			
+startIndexOfBatch = 0
+imagesPathArray = None
+imagesLabelsArray = []
 
+# Initializes the tensorflow graph
+tf.reset_default_graph()
+
+# The tendorflow graph components
+x = tf.placeholder(tf.float32, shape=[None, 784])
+W = tf.Variable(tf.truncated_normal([784, TOTAL_ELEMENTS]), dtype=tf.float32, name="weights_0")
+b = tf.Variable(tf.truncated_normal([TOTAL_ELEMENTS]), dtype=tf.float32, name="bias_0")
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# Training Parameters
+trainingRate = 0.005
+trainingLoops = 2000
+batchSize = 64
+
+# Tensorflow configuration to use CPU instead of GPU
+tf_config = tf.ConfigProto(
+		device_count = {'GPU': 0}
+		)
+
+# Matches each charachter to image folder name for training
 def getNumber(alphabet):
 	for i in range(TOTAL_ELEMENTS):
 		if(alphabet == folders[i]):
@@ -21,11 +55,7 @@ def getNumber(alphabet):
 	# This should happen if the alphabet doesnt matches list
 	assert(False)
 
-	# For Reference
-	# if(alphabet == "0"):
-	# 	return np.eye(TOTAL_ELEMENTS, dtype=np.float32)[0]
-
-	
+# For loading training images
 def getListOfImages():
 	global folders
 	global root
@@ -43,6 +73,7 @@ def getListOfImages():
 			allImagesLabelsArray = np.append(allImagesLabelsArray, currentAlphabetFolder)
 	return allImagesArray, allImagesLabelsArray
 
+# Shuffling images for training
 def shuffleImagesPath(imagesPathArray, imagesLabelsArray):
 	print("Size of imagesPathArray is: ", len(imagesPathArray))
 	for i in range(0, 100000):
@@ -54,7 +85,8 @@ def shuffleImagesPath(imagesPathArray, imagesLabelsArray):
 		imagesLabelsArray[randomIndex1], imagesLabelsArray[randomIndex2] = imagesLabelsArray[randomIndex2], imagesLabelsArray[randomIndex1]
 	return imagesPathArray, imagesLabelsArray
 
-	
+
+# This function returns the batch of images to be trained at each step
 def getBatchOfLetterImages(batchSize=64):
 	global startIndexOfBatch
 	global imagesPathArray
@@ -92,43 +124,31 @@ def getBatchOfLetterImages(batchSize=64):
 				except:
 					print("Unexpected Image, it's okay, skipping ({})".format(str(pathToImage)))
 			i+=1
-					
-startIndexOfBatch = 0
-imagesPathArray = None
-imagesLabelsArray = []
 
-
-tf.reset_default_graph()
-
-x = tf.placeholder(tf.float32, shape=[None, 784])
-W = tf.Variable(tf.truncated_normal([784, TOTAL_ELEMENTS]), dtype=tf.float32, name="weights_0")
-b = tf.Variable(tf.truncated_normal([TOTAL_ELEMENTS]), dtype=tf.float32, name="bias_0")
-y = tf.nn.softmax(tf.matmul(x, W) + b)
-
-trainingRate = 0.005
-trainingLoops = 2000
-batchSize = 64
-
-tf_config = tf.ConfigProto(
-		device_count = {'GPU': 0}
-		)
-
+# This is the function to begin the training process
 def BeginTraining():
+	# In order to reference global variables, 'global' keyword is used
 	global tf_config
 	global imagesPathArray
 	global imagesLabelsArray
 
+	# The following reads and shuffles the training images
 	imagesPathArray, imagesLabelsArray = getListOfImages()
 	imagesPathArray, imagesLabelsArray = shuffleImagesPath(imagesPathArray, imagesLabelsArray)
 	
+	# Here we specify the neural net output
 	yTrained = tf.placeholder(tf.float32, [None, TOTAL_ELEMENTS])
 
+	# This is the error function
 	crossEntropy = -tf.reduce_sum(yTrained * tf.log(y))
 
+	# This variable represents each training step
 	trainStep = tf.train.GradientDescentOptimizer(trainingRate).minimize(crossEntropy)
 
+	# This object saves the model when training is completed
 	saver = tf.train.Saver()
 
+	# Here we create a tensorflow session and start training
 	with tf.Session(config=tf_config) as session:
 		session.run(tf.global_variables_initializer())
 		for i in range(0, trainingLoops):
@@ -141,4 +161,7 @@ def BeginTraining():
 		print("Model saved at: " ,savedPath)
 
 if __name__ == '__main__':
+	# In python each .py file can be imported as library or run independently
+	# If run independently it comes here in __main__
+	# Therefore any following code will be executed
 	BeginTraining()
